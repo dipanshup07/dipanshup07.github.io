a853a4c811c87181b9f10ddef9a0f71d88814788
dipanshup07 <48696209+dipanshup07@users.noreply.github.com>
2020-12-31 16:35:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function Resume(){
+    window.open("https://drive.google.com/file/d/1LExDSufcMypOq3rh9jCqEuMwBwdJVEti/view?usp=sharing");
+};
+
+function Gitlab(){
+    window.open("https://gitlab-gl.stackroute.in/dipanshu.pimplikar");
+};
+
+function Github(){
+    window.open("https://github.com/dipanshup07");
+};
+
+function Twitter(){
+    window.open("https://twitter.com/dipanshup07");
+};
+
+function LinkedIn(){
+    window.open("https://www.linkedin.com/in/dipanshu-pimplikar-6b0854147/");
+};
